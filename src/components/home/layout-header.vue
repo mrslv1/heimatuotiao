7d292c4c568aafd27cb3a8bf243f3ece5c21c19f
lvxiaopeng <lvxiaopeng@outlook.com>
2019-12-21 22:34:26 +0800

    ## 黑马头条PC-主页模块-头部信息查询

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -7,11 +7,11 @@
       </el-col>
       <el-col class="right" :span="12">
           <el-row type="flex" justify="end" align="middle">
-              <img src="../../assets/img/2020.png" alt="">
+              <img :src="userlnfo.photo ? userlnfo.photo : '../../assets/img/2020.png'" alt="">
               <!-- 下拉菜单 -->
               <el-dropdown>
                   <!-- 匿名插槽  下拉菜单的内容 -->
-                <span>精武门陈真</span>
+                <span>{{userlnfo.mame}}</span>
                 <el-dropdown-menu slot="dropdown">
                     <el-dropdown-item>个人信息</el-dropdown-item>
                     <el-dropdown-item>git管理</el-dropdown-item>
@@ -26,8 +26,26 @@
 </template>
 
 <script>
-export default {
 
+export default {
+  data () {
+    return {
+      userlnfo: {}, // 定义一个用户对象
+      defaultlmg: require('../../assets/img/2020.png') // 先将图片传换成了一个变量
+    }
+  },
+  created () {
+    let token = localStorage.getItem('user-token') // 获取用户令牌
+    this.$axios({
+      url: '/user/profile',
+      headers: {
+        // header 的参数
+        Authorization: `Bearer ${token} `
+      }
+    }).then(result => {
+      this.userlnfo = result.data.data
+    })
+  }
 }
 </script>
 
