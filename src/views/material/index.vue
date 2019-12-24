c4a8cb4e2492dff74d6931306294e9e3661d74b8
lvxiaopeng <lvxiaopeng@outlook.com>
2019-12-24 20:01:29 +0800

    素材管理-新建页面-挂载路由

---
+++ src/views/material/index.vue
@@ -0,0 +1,22 @@
+<template>
+<!-- 素材管理 -->
+  <el-card>
+      <!-- 头部内容 -->
+      <bread-crumb slot="header">
+      <template slot="title">
+          素材管理
+      </template>
+
+      </bread-crumb>
+  </el-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
